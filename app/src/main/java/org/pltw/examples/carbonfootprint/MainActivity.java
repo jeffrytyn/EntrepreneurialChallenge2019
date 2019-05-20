554f0feef66ef82cbd1d60357922ab9f0adcbced
jeffrytyn <jeffreytian01@gmail.com>
2019-05-20 09:50:55 -0700

    changes

--- app/src/main/java/org/pltw/examples/carbonfootprint/MainActivity.java
+++ app/src/main/java/org/pltw/examples/carbonfootprint/MainActivity.java
@@ -1,13 +1,17 @@
 package org.pltw.examples.carbonfootprint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-TextView username;
-TextView password;
+private EditText username;
+private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +21,16 @@ TextView password;
 
     public void toHome(View view) {
         username = findViewById(R.id.Username);
+        String usernameString = username.getText().toString();
         password = findViewById(R.id.Password);
-        if(username.getText()!=null && password.getText()!=null) {
-            
+        String passwordString = password.getText().toString();
+        if(!TextUtils.isEmpty(usernameString)&&!TextUtils.isEmpty(passwordString)) {
+            Intent intent = new Intent(this, HomeScreen.class);
+            startActivity(intent);
+        }else{
+            Toast toast = Toast.makeText(getApplicationContext(), "Enter username and password", Toast.LENGTH_SHORT);
+            toast.show();
+
         }
     }
 }
